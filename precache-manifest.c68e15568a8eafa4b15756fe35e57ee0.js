f5430f4e7f6bd98170b5cc462f69ab82c09754eb
Waszek <omen20@op.pl>
2020-03-10 12:40:16 +0100

    Updates

---
+++ precache-manifest.c68e15568a8eafa4b15756fe35e57ee0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c6f7c25ef7ae24100aaed313ddc9a17",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "3d3de2638c6f8d45010a",
+    "url": "/weather-app/static/css/main.4f17a359.chunk.css"
+  },
+  {
+    "revision": "3e61278895e7faabc0e2",
+    "url": "/weather-app/static/js/2.7fc9d52a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather-app/static/js/2.7fc9d52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d3de2638c6f8d45010a",
+    "url": "/weather-app/static/js/main.f882332d.chunk.js"
+  },
+  {
+    "revision": "089fb1fee07ab9da1ce8",
+    "url": "/weather-app/static/js/runtime-main.029e69d5.js"
+  },
+  {
+    "revision": "cfcf8d9bec972952df2f382e342ce455",
+    "url": "/weather-app/static/media/background2.cfcf8d9b.jpg"
+  }
+]);
